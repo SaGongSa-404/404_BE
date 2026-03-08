d746e5cad6020b6c80e06ca5a2bcdeeffb4ce2d2
Felta <jae@DESKTOP-1UKCV62.localdomain>
2026-03-09 00:16:11 +0900

    feat:소셜 로그인-구글/카카오/네이버

---
+++ src/main/java/com/example/oauthsocialtest/OauthSocialTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthsocialtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSocialTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthSocialTestApplication.class, args);
+	}
+
+}
