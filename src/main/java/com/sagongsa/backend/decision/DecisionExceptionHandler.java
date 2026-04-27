4f6ab0a5b57194516d6fa11ed60b03abe9a8f4b3
박재홍 <105151063+PHJ2000@users.noreply.github.com>
2026-04-28 03:48:56 +0900

    fix: 결정 API 예외 처리 범위 제한

--- src/main/java/com/sagongsa/backend/decision/DecisionExceptionHandler.java
+++ src/main/java/com/sagongsa/backend/decision/DecisionExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(assignableTypes = DecisionController.class)
 public class DecisionExceptionHandler {
 
 	@ExceptionHandler(DecisionBadRequestException.class)
